de99d7bc53760fded700fae4cb59773210b4e0c6
marsclaraa <mariaclaralopesdemaria@gmail.com>
2025-08-12 15:45:12 -0300

    atualizações - 12/08

---
+++ app/Models/Sensor.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sensor extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'ambiente_id',
+        'codigo',
+        'tipo',
+        'descricao',
+        'status'
+    ];
+
+    public function registros(){
+        return $this->hasMany(Registro::class);
+    }
+}
