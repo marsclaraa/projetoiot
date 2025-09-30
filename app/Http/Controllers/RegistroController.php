476f87dc11ec44af425bfeae5043bbe5c26e5bc9
marsclaraa <mariaclaralopesdemaria@gmail.com>
2025-09-30 13:27:52 -0300

    Iot para API

--- app/Http/Controllers/RegistroController.php
+++ app/Http/Controllers/RegistroController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\RegistroRequest;
 use App\Models\Registro;
+use App\Models\Sensor;
 use Illuminate\Http\Request;
 
 class RegistroController extends Controller
 {
     public function store (RegistroRequest $request){
+
+ $sensor = Sensor::where('codigo', 'like'. $request->codigo)->first();
+if($sensor == null){
+    return response()->json([
+        'status' =>false,
+        'message' => 'nao foi possivel'
+    ]);
+}
         $registro = Registro::create([
             'sensor_id' => $request-> sensor_id,
             'valor'=> $request->valor,
